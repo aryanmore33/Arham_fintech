e39059181e2486a7d2e69b3dac3ab41671ddcf5e
aryanmore33 <aryanmore.1359@gmail.com>
2026-08-15 16:01:05 +0530

    added production in knexfile

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -26,6 +26,27 @@ module.exports = {
     },
   },
 
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+      database: process.env.PGDATABASE,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
+  },
+
   // staging: {
   //   client: 'postgresql',
   //   connection: {
